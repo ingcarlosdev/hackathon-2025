c8dd26f0fbacc4e82c8332c6ecc7bfb53ac96f5c
esn8der <ballesta.esneider@gmail.com>
2025-10-29 09:50:09 -0500

    Actualiza .gitignore, agrega nuevos endpoints en main.py para consultar fincas y lotes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,9 @@
 # app/main.py
-from fastapi import FastAPI, UploadFile, File
+import os
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from app.validators import validar_csv  # 👈 tu función personalizada
+import httpx
 
 # ⚙️ Inicializar la aplicación FastAPI
 app = FastAPI(title="Geo-Validador API", version="1.0")
@@ -19,6 +21,43 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+SIOMA_API_BASE = os.getenv("SIOMA_API_BASE")
+SIOMA_API_KEY = os.getenv("SIOMA_API_KEY")
+
+# 🔹 Endpoint para obtener todas las fincas
+@app.get("/fincas/")
+async def get_fincas():
+    try:
+        headers = {
+            "Authorization": f"{SIOMA_API_KEY}",
+            "Content-Type": "application/json",
+            "tipo-sujetos": "[1]"
+        }
+        async with httpx.AsyncClient() as client:
+            res = await client.get(f"{SIOMA_API_BASE}/4/usuarios/sujetos", headers=headers)
+            res.raise_for_status()
+            data = res.json()
+        return {"status": "ok", "fincas": data}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
+@app.get("/lotes/{finca_id}")
+async def get_lotes(finca_id: str):
+    try:
+        headers = {
+            "Authorization": f"{SIOMA_API_KEY}",
+            "Content-Type": "application/json",
+            "tipo-sujetos": "[3]"
+        }
+        async with httpx.AsyncClient() as client:
+            res = await client.get(f"{SIOMA_API_BASE}/4/usuarios/sujetos/{finca_id}", headers=headers)
+            res.raise_for_status()
+            data = res.json()
+        return {"status": "ok", "lotes": data}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
+
 
 # 💾 Endpoint para validar CSV
 @app.post("/validar-csv/")
@@ -28,3 +67,5 @@ async def validar_csv_endpoint(file: UploadFile = File(...)):
         return resultado
     except Exception as e:
         return {"status": "error", "message": str(e)}
+    
+
--- backend/app/test-validar.py
+++ backend/app/test-validar.py
@@ -2,7 +2,7 @@ from validators import validar_csv
 
 
 if __name__ == "__main__":
-    ruta_csv = "/home/esn8der/Documentos/PC/hackathon/plantaciones.csv"  # coloca aquí tu archivo CSV
+    ruta_csv = "c://Users//udea-hackathon//Desktop//test//plantaciones.csv"  # coloca aquí tu archivo CSV
     resultado = validar_csv(ruta_csv)
     
     # Mostrar el resultado de manera legible
